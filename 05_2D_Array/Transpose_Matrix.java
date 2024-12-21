97468e4ed2331b591bd8059f0803514045f9fc1d
Ujjwal Prajapati <ujjwalprajapati154@gmail.com>
2024-12-21 12:47:33 +0530

    2D Array

---
+++ 05_2D_Array/Transpose_Matrix.java
@@ -0,0 +1,26 @@
+public class Transpose_Matrix {
+    public static void transpose(int matrix[][]) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+
+        int transpose[][] = new int[col][row];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                transpose[j][i] = matrix[i][j];
+            }
+        }
+
+        for (int i = 0; i < col; i++) {
+            for (int j = 0; j < row; j++) {
+                System.out.print(transpose[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int matrix[][] = { { 1, 2, 3 },
+                { 4, 5, 6 } };
+        transpose(matrix);
+    }
+}
