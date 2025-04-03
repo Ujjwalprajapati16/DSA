70859de6b355e45df2f0f73836491336a409dd5d
Ujjwal Prajapati <ujjwalprajapati154@gmail.com>
2025-04-03 11:22:12 +0530

    Add implementations for SubTree of another tree and Top View of a binary tree

---
+++ 17_Binary_Tree/SubTree_of_another_tree.java
@@ -0,0 +1,69 @@
+
+public class SubTree_of_another_tree {
+
+    static class Node {
+
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static boolean isIdentical(Node root, Node subtree) {
+        if (root == null && subtree == null) {
+            return true;
+        } else if (root == null || subtree == null || root.data != subtree.data) {
+            return false;
+        }
+
+        if (!isIdentical(root.left, subtree.left)) {
+            return false;
+        }
+
+        if (!isIdentical(root.right, subtree.right)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static boolean isSubtree(Node root, Node subtree) {
+        // base case
+        if (root == null) {
+            return subtree == null;
+        }
+        if (subtree == null) {
+            return false;
+        }
+
+        if (root.data == subtree.data) {
+            if (isIdentical(root, subtree)) {
+                return true;
+            }
+        }
+
+        return isSubtree(root.left, subtree) || isSubtree(root.right, subtree);
+    }
+
+
+    public static void main(String[] args) {
+
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        Node subTree = new Node(2);
+        subTree.left = new Node(4);
+        subTree.right = new Node(5);
+
+        System.out.println("Is subtree: " + isSubtree(root, subTree)); // true
+    }
+}
---
+++ 17_Binary_Tree/TopView.java
@@ -0,0 +1,126 @@
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class TopView {
+
+    static class Node {
+
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class Info {
+
+        Node node;
+        int hd; // horizontal distance from the root
+
+        Info(Node node, int hd) {
+            this.node = node;
+            this.hd = hd;
+        }
+    }
+
+    public static void topView(Node node) {
+        //Level Order
+        Queue<Info> queue = new LinkedList<>();
+        HashMap<Integer, Node> map = new HashMap<>(); // to store the first node at each horizontal distance
+
+        int min = 0, max = 0;
+        queue.add(new Info(node, 0));
+        queue.add(null);
+
+        while (!queue.isEmpty()) {
+            Info curr = queue.remove();
+            if (curr == null) {
+                if (queue.isEmpty()) {
+                    break;
+                } else {
+                    queue.add(null);
+                }
+            } else {
+
+                if (!map.containsKey(curr.hd)) {
+                    map.put(curr.hd, curr.node);
+                }
+
+                if (curr.node.left != null) {
+                    queue.add(new Info(curr.node.left, curr.hd - 1));
+                    min = Math.min(min, curr.hd - 1);
+                }
+
+                if (curr.node.right != null) {
+                    queue.add(new Info(curr.node.right, curr.hd + 1));
+                    max = Math.max(max, curr.hd + 1);
+                }
+            }
+        }
+
+        for (int i = min; i <= max; i++) {
+            if (map.containsKey(i)) {
+                System.out.print(map.get(i).data + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void bottomView(Node root) {
+        Queue<Info> queue = new LinkedList<>();
+        HashMap<Integer, Node> map = new HashMap<>();
+
+        int min = 0, max = 0;
+        queue.add(new Info(root, 0));
+        queue.add(null);
+
+        while (!queue.isEmpty()) {
+            Info curr = queue.remove();
+            if (curr == null) {
+                if (queue.isEmpty()) {
+                    break;
+                } else {
+                    queue.add(null);
+                }
+            } else {
+                map.put(curr.hd, curr.node);
+                if (curr.node.left != null) {
+                    queue.add(new Info(curr.node.left, curr.hd - 1));
+                    min = Math.min(min, curr.hd - 1);
+                }
+
+                if (curr.node.right != null) {
+                    queue.add(new Info(curr.node.right, curr.hd + 1));
+                    max = Math.max(max, curr.hd + 1);
+                }
+
+            }
+        }
+
+        for (int i = min; i <= max; i++) {
+            if (map.containsKey(i)) {
+                System.out.print(map.get(i).data + " ");
+            }
+        }
+        System.out.println();
+
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(4);
+        root.right.right = new Node(6);
+
+        topView(root);
+        bottomView(root);
+    }
+}
